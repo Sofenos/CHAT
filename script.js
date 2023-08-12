8dae0faed59f62f07a5a61c7a1e700d5c0416c19
AINNASSE Soufiane <soufianainos@gmail.com>
2023-08-12 17:20:18 +0100

    lol

--- script.js
+++ script.js
@@ -6,22 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
 
         const topicInput = document.getElementById("topic").value;
+        console.log("Topic input:", topicInput);
 
         try {
-            const response = await fetch("/generate", {
+            const response = await fetch("http://localhost:5000/generate", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({ topic: topicInput }),
             });
-
+            
+            console.log("Fetch URL:", "http://localhost:5000/generate");
+            
             const data = await response.json();
+            console.log("Response:", data);
             generatedContent.innerText = data.generated_content;
         } catch (error) {
             console.error("Error:", error);
         }
     });
 });
-console.log("Topic input:", topicInput);
 
